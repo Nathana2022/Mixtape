7eb4d717a5d503efe80c994db5e50fac30930223
Nathana2022 <nathanaeldon495@gmail.com>
2023-12-15 20:15:38 +0100

    Style : management the style of main

--- src/SongList.jsx
+++ src/SongList.jsx
@@ -1,10 +1,24 @@
-import React from "react";
+import React, {useContext} from "react";
+import {Song} from "./Song";
+import {MixtapeContext} from "./MixtapeContext";
 
-export const SongList = () => {
-  // Your code here! ✨
+
+
+export const SongList = ({songs}) => {
+  const {genre, sortOrder} = useContext(MixtapeContext)
   return (
-    <>
-      <h2>TODO: Update taste in music...?</h2>
-    </>
+    <div>
+       {songs.filter(song => genre === 'all' || song.genre === genre).sort((a, b) => {
+        if (sortOrder === true) {
+          return a.name.localeCompare(b.name);
+        } else {
+          return b.name.localeCompare(a.name);
+        }
+      }).map(song => (
+        <Song key={song.name} {...song} />
+      ))
+      }
+      {/* <h2>TODO: Update taste in music...?</h2> */}
+    </div>
   );
 };
